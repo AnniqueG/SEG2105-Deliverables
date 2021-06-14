a160f6af81964b928f7e6793a4324e572b392203
ehann092 <84337449+ehann092@users.noreply.github.com>
2021-06-13 23:25:20 -0400

    Update ManageAdminActivity.java
    Just added the variables and initialized the buttons

--- ManageAdminActivity.java
+++ ManageAdminActivity.java
@@ -3,12 +3,34 @@ package com.example.courseselectionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ManageAdminActivity extends AppCompatActivity {
+
+    Button viewBTN;
+    Button editBTN;
+    Button deleteBTN;
+    Button createBTN;
+    EditText currNameTXT;
+    EditText currCodeTXT;
+    EditText newNameTXT;
+    EditText newCodeTXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_admin);
+
+        //
+        viewBTN = findViewById(R.id.viewAllCourses);
+        editBTN = findViewById(R.id.editCourse);
+        deleteBTN = findViewById(R.id.deleteCourse);
+        createBTN = findViewById(R.id.createCourse);
+
+        String currName = currNameTXT.getText().toString();
+        String currCode = currCodeTXT.getText().toString();
+        String newName = newNameTXT.getText().toString();
+        String newCode = newCodeTXT.getText().toString();
     }
 }
