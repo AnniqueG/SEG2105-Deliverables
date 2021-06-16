91c584fcd5480849bfa0e8d34c50e9858651168b
ehann092 <84337449+ehann092@users.noreply.github.com>
2021-06-16 11:46:53 -0400

    Create User.java
    This is the user class just has all the stuff we put in our UML diagram. It's used to add/delete users

---
+++ User.java
@@ -0,0 +1,35 @@
+package com.example.courseselectionapp;
+
+
+public class User {
+
+    private String userName;
+    private String password;
+    private String role;
+
+    public User(){
+
+    }
+
+    public User(String userName, String password, String role){
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+
+    //Setters and getters
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+
+    public String getRole(){
+        return this.role;
+    }
+}
