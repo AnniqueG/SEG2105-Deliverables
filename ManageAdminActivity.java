47572c893c93003cb2bcd150a03109f2f6f1c081
ehann092 <84337449+ehann092@users.noreply.github.com>
2021-06-21 12:40:34 -0400

    Update ManageAdminActivity.java
    Added comments

--- ManageAdminActivity.java
+++ ManageAdminActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+/**
+*Class for the managing courses page 
+*/
 public class ManageAdminActivity extends AppCompatActivity {
 
     Button viewBTN;
@@ -22,7 +25,7 @@ public class ManageAdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_admin);
 
-        //
+        //Set all the buttons to their ids
         viewBTN = findViewById(R.id.viewAllCourses);
         editBTN = findViewById(R.id.editCourse);
         deleteBTN = findViewById(R.id.deleteCourse);
