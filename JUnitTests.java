297177ab2305e04afbc945d44530e7b943e36dfe
AnniqueG <77369698+AnniqueG@users.noreply.github.com>
2021-07-15 14:02:40 -0400

    Update JUnitTests.java

--- JUnitTests.java
+++ JUnitTests.java
@@ -6,14 +6,16 @@ import static org.junit.Assert.*;
 import com.example.courseselectionapp.InstructorSuccessActivity;
 import com.example.courseselectionapp.SignInAsActivity;
 
-public class JUnitTests {
+public class JUnitTests{
 
+    //deliverable 2
     @Test
     public void testIsAlphaNumericReturnFalse(){
         assertFalse(InstructorSuccessActivity.isAlphaNumeric("6e_wpI!!"));
 
     }
 
+    //deliverable 2
     @Test
     public void testIsAlphaNumericReturnTrue(){
         assertTrue(InstructorSuccessActivity.isAlphaNumeric("Annique55"));
@@ -25,18 +27,63 @@ public class JUnitTests {
 //
 //    }
 
+    //deliverable 2
     @Test
     public void testIsNumberFalse(){
         assertFalse(InstructorSuccessActivity.isNumber("-33"));
     }
 
+    //deliverable 2
     @Test
     public void testIsNumberTrue(){
         assertTrue(InstructorSuccessActivity.isNumber("54"));
     }
 
+    //deliverable 2
     @Test
     public void testGetRole(){
         assertNull(SignInAsActivity.getRole());
     }
+
+    //deliverable 3
+
+    @Test
+    public void testLookupCourseCourseDB(){
+        Course course = new Course("ENG",1100, 25,"2:00","wednesday","engineering","annique");
+        StudentSuccessActivity success = new StudentSuccessActivity();
+        assertEquals(course, success.lookupCourseCourseDB(course.getCourseName()));
+    }
+
+    //deliverable 3
+    @Test
+    public void testLookupCourseCodeCourseDB(){
+        Course course = new Course("FRA",2500, 25,"2:00","wednesday","french","annique");
+        StudentSuccessActivity success = new StudentSuccessActivity();
+        assertEquals(course, success.lookupCourseCodeCourseDB(course.getCourseCode()));
+    }
+
+    //deliverable 3
+    @Test
+    public void testLookupCourseDayCourseDB(){
+        Course course = new Course("SEG",2105,45,"2:00","thursday","software","annique");
+        StudentSuccessActivity success = new StudentSuccessActivity();
+        assertEquals(course,success.lookupCourseDayCourseDB(course.getDays()));
+    }
+
+    //deliverable 3
+    @Test
+    public void testUnEnroll(){
+        Course course = new Course("MAT",2748, 25,"2:00","wednesday","math","annique");
+        StudentSuccessActivity success = new StudentSuccessActivity();
+    }
+
+    //deliverable 3
+    @Test
+    public void testNewCourse() {
+        Course course = new Course("CSI", 2110, 25, "2:00", "wednesday", "computer", "annique");
+        StudentSuccessActivity success = new StudentSuccessActivity();
+
+    }
+
+
 }
