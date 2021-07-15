87f85463ac04ef60e107db0c7db8e8d142bdd2f6
AnniqueG <77369698+AnniqueG@users.noreply.github.com>
2021-07-15 14:43:46 -0400

    Update JUnitTests.java

--- JUnitTests.java
+++ JUnitTests.java
@@ -51,7 +51,7 @@ public class JUnitTests{
     public void testLookupCourseCourseDB(){
         Course course = new Course("ENG",1100, 25,"2:00","wednesday","engineering","annique");
         StudentSuccessActivity success = new StudentSuccessActivity();
-        assertEquals(course, success.lookupCourseCourseDB(course.getCourseName()));
+        assertSame(course, success.lookupCourseCourseDB(course.getCourseName()));
     }
 
     //deliverable 3
@@ -59,7 +59,7 @@ public class JUnitTests{
     public void testLookupCourseCodeCourseDB(){
         Course course = new Course("FRA",2500, 25,"2:00","wednesday","french","annique");
         StudentSuccessActivity success = new StudentSuccessActivity();
-        assertEquals(course, success.lookupCourseCodeCourseDB(course.getCourseCode()));
+        assertSame(course, success.lookupCourseCodeCourseDB(course.getCourseCode()));
     }
 
     //deliverable 3
@@ -67,21 +67,24 @@ public class JUnitTests{
     public void testLookupCourseDayCourseDB(){
         Course course = new Course("SEG",2105,45,"2:00","thursday","software","annique");
         StudentSuccessActivity success = new StudentSuccessActivity();
-        assertEquals(course,success.lookupCourseDayCourseDB(course.getDays()));
+        assertSame(course,success.lookupCourseDayCourseDB(course.getDays()));
     }
 
     //deliverable 3
     @Test
-    public void testUnEnroll(){
+    public void testLookupCourseInstructor(){
         Course course = new Course("MAT",2748, 25,"2:00","wednesday","math","annique");
         StudentSuccessActivity success = new StudentSuccessActivity();
+        assertSame(course,success.lookupCourseIntructorDB(course.getCourseName()));
     }
 
     //deliverable 3
     @Test
-    public void testNewCourse() {
+    public void testDeleteCourse() {
         Course course = new Course("CSI", 2110, 25, "2:00", "wednesday", "computer", "annique");
-        StudentSuccessActivity success = new StudentSuccessActivity();
+        MyDBHandlerStudent dbStudent = new MyDBHandlerStudent();
+        assertTrue(dbStudent.deleteCourse(course.getCourseName()));
+
 
     }
 
